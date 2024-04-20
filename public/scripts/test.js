08cb242973c32a8be9a118136da2318823f51c8c
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-04-20 16:58:19 -0700

    extra changes

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,17 +1,1 @@
-const divs = document.querySelectorAll("div");
 
-if (divs) {
-  console.log(divs.length);
-  const length = document.createElement("p");
-  length.textContent = divs.length;
-  length.classList.add("length");
-
-  //   const div_arr = [...divs];
-  //   div_arr.forEach((item) => {
-  //     item.textContent = "hello!";
-  //   });
-}
-
-chrome.storage.onChanged.addListener(async (tabId, info, tab) => {
-  console.log("updated!");
-});
