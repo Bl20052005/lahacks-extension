ca8f95b4c0e38f72117b358a4e3f92fabbaed60c
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-04-19 23:36:07 -0700

    initial extension work

---
+++ scripts/test.js
@@ -0,0 +1,17 @@
+const divs = document.querySelectorAll("div");
+
+if (divs) {
+  console.log(divs.length);
+  const length = document.createElement("p");
+  length.textContent = divs.length;
+  length.classList.add("length");
+
+  //   const div_arr = [...divs];
+  //   div_arr.forEach((item) => {
+  //     item.textContent = "hello!";
+  //   });
+}
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  console.log("updated!");
+});
