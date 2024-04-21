07046265a2783ef45d437e82bb29c448056933f8
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2024-04-20 21:58:22 -0700

    some new changes to UI

--- src/AlternativeBlock.jsx
+++ src/AlternativeBlock.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import "./AlternativeBlock.css";
 
-function AlternativeBlock({ imgLink, imgAlt, description, price }) {
+function AlternativeBlock({ imgLink, imgAlt, description, price, blockLink }) {
   return (
     <div className="alterative-block">
       <img
@@ -11,6 +11,12 @@ function AlternativeBlock({ imgLink, imgAlt, description, price }) {
       />
       <p className="alternative-description">{description}</p>
       <p className="alternative-price">${price}</p>
+      <a
+        className="alternative-block-link"
+        href={blockLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
     </div>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       imgAlt: "sussy",
       description: "very SUStainable",
       price: "99999999.99",
+      blockLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     },
     {
       imgLink:
@@ -22,6 +23,7 @@ function App() {
       imgAlt: "sussy",
       description: "very SUStainable",
       price: "99999999.99",
+      blockLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     },
     {
       imgLink:
@@ -29,6 +31,7 @@ function App() {
       imgAlt: "sussy",
       description: "very SUStainable",
       price: "99999999.99",
+      blockLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     },
     {
       imgLink:
@@ -36,6 +39,7 @@ function App() {
       imgAlt: "sussy",
       description: "very SUStainable",
       price: "99999999.99",
+      blockLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     },
   ]);
 
@@ -161,7 +165,11 @@ function App() {
 
       <header className="header">
         <div className="logo">
-          <svg className="logo-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <svg
+            className="logo-svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
             <path d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5h88c8.8 0 16 7.2 16 16s-7.2 16-16 16H288 216s0 0 0 0c-16.6 0-32.7 1.9-48.3 5.4c-25.9 5.9-49.9 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440v16c0 13.3 10.7 24 24 24s24-10.7 24-24V440c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z" />
           </svg>
         </div>
@@ -170,11 +178,25 @@ function App() {
 
       <div className="product-title">product title</div>
 
-      <div className="rating">
+      {/* <div className="rating">
         <div className="rating-circle yellow">
           <div className="rating-message">Maybe</div>
         </div>
+      </div> */}
+
+      <div className="rating-container">
+        <div className="rating-arrow">
+          <div className="rating-arrow-body"></div>
+          <div className="rating-arrow-triangle"></div>
+        </div>
+        <div className="rating-circle">
+          <div className="black-rating-line-1"></div>
+          <div className="rating-blocking-circle"></div>
+          <div className="black-rating-line-2"></div>
+        </div>
       </div>
+
+      <div className="rating-result">Not sustainable</div>
 
       <div className="product-container">
         <div className="description-title">description:</div>
@@ -182,7 +204,7 @@ function App() {
           This product has yet to be implemented!
         </div>
       </div>
-      
+
       <div className="division-line"></div>
 
       <div className="alternatives">
@@ -194,6 +216,7 @@ function App() {
               imgAlt={info?.imgAlt}
               description={info?.description}
               price={info?.price}
+              blockLink={info?.blockLink}
             />
           );
         })}
